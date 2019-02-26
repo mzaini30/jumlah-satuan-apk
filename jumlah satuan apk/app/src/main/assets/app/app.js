61dcb528313624aed4115a34ed2ed4dd46d8abb1
mzaini30 <muhzaini30@gmail.com>
2019-02-26 08:38:42 +0800

    oke

--- jumlah satuan apk/app/src/main/assets/app/app.js
+++ jumlah satuan apk/app/src/main/assets/app/app.js
@@ -9,6 +9,8 @@ $('.angka').keyup(function(){
 	if ($('.hasil').html() == ''){
 		$('.hasil').html('0')
 	}
+
+	$('.angka').val($('.angka').val().replace(/(\d{5})/g, '$&-'))
 })
 
 $('.hapus').click(function(){
@@ -20,8 +22,8 @@ $('.navbar-collapse a').click(function(){
 	$('.navbar-toggle').click()
 })
 
-new Cleave('.angka', {
-    // numeral: true,
-    delimiter: '-',
-    blocks: [5] // masih error di bagian ini
-})
+// new Cleave('.angka', {
+//     // numeral: true,
+//     delimiter: '-',
+//     blocks: [5] // masih error di bagian ini
+// })
